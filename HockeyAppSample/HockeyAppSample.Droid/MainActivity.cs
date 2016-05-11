aa9c8231f9fba8ff401ae99c0ee1f2aec8aa66a8
Dylan Wegner <dwegner@crinet.com>
2016-05-11 08:35:50 -0500

    Add project files.

---
+++ HockeyAppSample/HockeyAppSample.Droid/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using HockeyApp;
+using HockeyApp.Metrics;
+
+namespace HockeyAppSample.Droid
+{
+	[Activity (Label = "HockeyAppSample", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+        //Michelle's
+        public const string API_KEY = "6052a295ea384e6bbbd30572d87504f7";
+
+        //Dylan's
+        public const string Dylan_API_KEY = "89ab15ddbfdb436e90f10e77383adcbb";
+
+        protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+            //Adding crash reporting
+            CrashManager.Register(this, Dylan_API_KEY);
+
+            //Adding user metrics accroding to https://github.com/bitstadium/HockeySDK-Xamarin#user-metrics
+            MetricsManager.Register(this, Application, Dylan_API_KEY);
+            MetricsManager.EnableUserMetrics();
+
+            global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new HockeyAppSample.App ());
+		}
+	}
+}
+
---
+++ HockeyAppSample/HockeyAppSample/MyPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using HockeyApp;
+using HockeyApp.Metrics;
+
+namespace HockeyAppSample
+{
+	public partial class MyPage : ContentPage
+	{
+		public MyPage ()
+		{
+			InitializeComponent ();
+		}
+
+        public void OnForceException_Clicked(object sender, EventArgs args)
+        {
+            throw new NotSupportedException();
+            //throw new Exception("You just forced and exception. Hopefully this makes it the hockeyapp dashboard.");
+        }
+
+    }
+}
